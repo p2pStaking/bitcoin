7d4d4c0443403d7ae92f928f94d64de08b78aed1
p2pstaking <admin@p2pstaking.org>
2024-04-09 09:32:26 +0200

    bitcoin-core + fulcrum docker images

---
+++ fulcrum/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash 
+
+[[ "$1" == "" ]] && echo "Usage $0 git_tag [docker_repo/image]" && exit 1 
+
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+BDIR=$(pwd)
+
+
+REPO=${2:-p2pstaking/fulcrum}
+
+DIR=$(mktemp -d)
+trap "rm -Rf $DIR" EXIT
+
+cd $DIR 
+git clone https://github.com/cculianu/Fulcrum.git
+cd Fulcrum
+
+DOCKER_VERSION=$( echo $1 |  egrep  -o '[0-9\.]{1,}')
+
+./contrib/build/build.sh linux $1
+
+FULCRUM_FILE=$PWD/dist/linux/Fulcrum
+cd $BDIR
+
+mv $FULCRUM_FILE . 
+docker build .  -t $REPO:$DOCKER_VERSION
+rm Fulcrum
+docker push $REPO:$DOCKER_VERSION
+
